c5ebd7b8ddbc660838da7b61a6b9c87e36217deb
guerrero-13@hotmail.es <gsegundoarias@A5PC18.ieszayas.org>
2015-04-27 13:12:42 +0200

    v 1.1
    Signed-off-by: guerrero-13@hotmail.es
    <gsegundoarias@A5PC18.ieszayas.org>

---
+++ zayasoft/src/zayasoft/MAIN.java
@@ -0,0 +1,11 @@
+package zayasoft;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MODULO1 m = new MODULO1();
+		m.imprimeN(10);
+	}
+
+}
