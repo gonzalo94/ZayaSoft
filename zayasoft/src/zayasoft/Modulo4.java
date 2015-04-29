c340a0bab1703c03ed0d49e3fadd0db189df66e5
guerrero-13@hotmail.es <gsegundoarias@A5PC18.ieszayas.org>
2015-04-29 10:02:17 +0200

    v 1.4

---
+++ zayasoft/src/zayasoft/Modulo4.java
@@ -0,0 +1,15 @@
+package zayasoft;
+
+public class Modulo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+
+int a=3;
+int b=4;
+int multi= a*b;
+
+System.out.println("El numero es:" +multi );}}
