b540ca98f64efa8dc0ea275fc58752a16da3ecc2
guerrero-13@hotmail.es <gsegundoarias@A5PC18.ieszayas.org>
2015-04-29 10:01:06 +0200

    v 1.3

---
+++ zayasoft/src/zayasoft/Modulo3.java
@@ -0,0 +1,14 @@
+package zayasoft;
+
+public class Modulo3 {
+
+	
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		int resta= a-b;
+		
+	System.out.println("El numero es:" +resta );
+}
+}
